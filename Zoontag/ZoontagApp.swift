3c855b88eb5fdf09a41769756dcfec638f4eec23
Luis R <theluisrivera@gmail.com>
2025-12-27 11:00:18 -0800

    Initial Commit

---
+++ Zoontag/ZoontagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoontagApp.swift
+//  Zoontag
+//
+//  Created by Luis on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ZoontagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
